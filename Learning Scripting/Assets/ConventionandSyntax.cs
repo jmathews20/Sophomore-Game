19e5e89ec952cb3270774314ab560fd135384097
Jared Mathews <jmathews20@hotmail.com>
2016-09-17 23:57:39 -0600

    Convention and Syntax
    Just showing how to name and order things in a script.  Created Name and Age input script

---
+++ Learning Scripting/Assets/ConventionandSyntax.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionandSyntax : MonoBehaviour {
+
+    //Name and Age Inputs
+    public string firstName;
+    public string lastName;
+    public int age = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //Console will print out your name and age
+        print ("My Name is " + firstName + lastName);
+        print ("I am " + age + " years old");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Console will determine if you are young or old (don't take it personally)
+
+        if (age > 40)
+        {
+            print("Wow your getting Old!");
+        }
+        else
+        {
+            print("Lookin Good!  Still young");
+        }
+
+	}
+}
