967e899b1c5889bad83970443e5899c5b75496a5
Jared Mathews <jmathews20@hotmail.com>
2016-09-19 13:58:48 -0600

    Level Recycle scripting in Unity

---
+++ Star Catcher/Assets/Level Scripts/RecycleLevelObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecycleLevelObject : MonoBehaviour {
+
+    private Vector3 newLocation;
+
+    void OnTriggerEnter()
+    {
+        newLocation.x = StaticVars.nextSectionPosition;
+        transform.position = newLocation;
+        StaticVars.nextSectionPosition += StaticVars.distance;
+
+    }
+
+
+}
