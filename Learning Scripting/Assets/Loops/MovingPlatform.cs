475763d5df2bd7c66aea9d5aa4e806c28f3d0d0f
Jared Mathews <jmathews20@hotmail.com>
2016-09-21 01:25:47 -0600

    While Loop
    Almost got it to work, but unity crashes still

---
+++ Learning Scripting/Assets/Loops/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public float speed = 0;
+    private Vector3 tempPos;
+    public float power = 1000f;
+    public float losePowerTime = 1f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+
+        power -= (losePowerTime * Time.deltaTime);
+
+        tempPos.x = speed * Time.deltaTime;
+        transform.Translate(tempPos);
+
+            while (power > 0)
+        {
+            speed = 1f;
+        }
+    }
+}
