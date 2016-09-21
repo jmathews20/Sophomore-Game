ccf9b57cc48cdcc2ed9bb8f7dc16f0bacff622ed
Jared Mathews <jmathews20@hotmail.com>
2016-09-21 13:58:28 -0600

    Endless Recycler created

--- Star Catcher/Assets/Level Scripts/RecycleLevelObject.cs
+++ Star Catcher/Assets/Level Scripts/RecycleLevelObject.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class RecycleLevelObject : MonoBehaviour {
 
     private Vector3 newLocation;
+    public List<Recycler> recyclableList;
+    private int i = 0;
+
+    void Start ()
+    {
+        recyclableList = new List<Recycler>();
+        Recycler.RecycleAction += RecycleActionHandler;
+    }
+
+    private void RecycleActionHandler(Recycler _r)
+    {
+        recyclableList.Add(_r);
+
+    }
 
     void OnTriggerEnter()
     {
         newLocation.x = StaticVars.nextSectionPosition;
-        transform.position = newLocation;
+        recyclableList[i].cube.position = newLocation;
         StaticVars.nextSectionPosition += StaticVars.distance;
-
+        if (i < recyclableList.Count-1)
+            i++;
     }
 
 
