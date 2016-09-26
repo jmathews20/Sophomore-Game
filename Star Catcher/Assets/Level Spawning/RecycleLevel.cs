1450bfdf955b11889ff32542934751adbb9c123f
Jared Mathews <jmathews20@hotmail.com>
2016-09-26 14:00:02 -0600

    Recycle Level, delagates n' Stuff

---
+++ Star Catcher/Assets/Level Spawning/RecycleLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RecycleLevel : MonoBehaviour {
+
+    private List<SendToRecycler> recycleList;
+    private Vector3 movePos;
+
+    void SendThisHandler (SendToRecycler _r)
+    {
+        recycleList.Add(_r);
+        print(recycleList.Count);
+    }
+
+	// Use this for initialization
+	void Start () {
+        recycleList = new List<SendToRecycler>();
+        SendToRecycler.SendThis += SendThisHandler;
+	}
+
+    void OnTriggerEnter ()
+    {
+        movePos.x = Statics.nextPosition;
+        recycleList[0].transform.position = movePos;
+        Statics.nextPosition += Statics.distance;
+    }
+}
---
+++ Star Catcher/Assets/Level Spawning/SendToRecycler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SendToRecycler : MonoBehaviour {
+
+    public static Action<SendToRecycler> SendThis;
+
+	// Use this for initialization
+	void Start () {
+        SendThis(this);
+	}
+	
+}
