97dde9293597c0e5af127ad31f01f4f0849b7812
Jared Mathews <jmathews20@hotmail.com>
2016-10-03 14:03:17 -0600

    Flippin Characters!!

---
+++ Star Catcher/Assets/Prototype #1/Level Scripts/CharacterFlip.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlip : MonoBehaviour {
+
+    public Transform characterArt;
+
+    public bool forward = true;
+
+    void FlipCharacter (bool _b)
+    {
+        characterArt.Rotate(0, 180, 0);
+        forward = _b;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.RightArrow) && forward)
+        {
+            FlipCharacter(false);
+            /*
+                characterArt.Rotate(0, 180, 0);
+            print("Forward");
+            forward = false;
+            */
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !forward)
+        {
+            FlipCharacter(true);
+            /*
+            characterArt.Rotate(0, 180, 0);
+            print("Backward");
+            forward = true;
+            */
+        }
+
+    }
+}
