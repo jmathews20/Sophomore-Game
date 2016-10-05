8b85e2595d36a5340f886872d82b58210d0b5f73
Jared Mathews <jmathews20@hotmail.com>
2016-10-05 13:57:44 -0600

    Character Flip using Delegates

---
+++ Star Catcher/Assets/Prototype #2/Prototype 2 Scripts/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UserInput : MonoBehaviour
+{
+
+    public static Action<KeyCode> userInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && userInput != null)
+        {
+            userInput(KeyCode.RightArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && userInput != null)
+        {
+            userInput(KeyCode.LeftArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && userInput != null)
+        {
+            userInput(KeyCode.RightArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && userInput != null)
+        {
+            userInput(KeyCode.LeftArrow);
+        }
+
+    }
+}
