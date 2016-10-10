2569219746e94fc7b060b9980e5fcf5389f6cdde
Jared Mathews <jmathews20@hotmail.com>
2016-10-10 13:59:38 -0600

    Workin AI Enemy Follow Player

---
+++ Star Catcher/Assets/Prototype #2/Prototype 2 Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public CharacterController myCC;
+    public float gravity = 1;
+    public Vector3 tempPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        tempPos.y -= gravity * Time.deltaTime;
+        myCC.Move(tempPos);
+
+	}
+}
