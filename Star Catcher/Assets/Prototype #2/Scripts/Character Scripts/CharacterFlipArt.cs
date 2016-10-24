dc050c8c7c5aa7501b6fbd1e50354a43d53caa63
Daffy <jmathews20@hotmail.com>
2016-10-24 13:57:06 -0600

    The game fer real

---
+++ Star Catcher/Assets/Prototype #2/Scripts/Character Scripts/CharacterFlipArt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlipArt : MonoBehaviour {
+
+	public Transform characterArt;
+	public bool forward = true;
+
+
+	void FlipCharacter (KeyCode _keyCode)
+	{
+		switch (_keyCode)
+		{
+		case KeyCode.RightArrow:
+			if (forward) {
+				characterArt.Rotate(0, 180, 0);
+				forward = false;
+			}
+			break;
+
+		case KeyCode.LeftArrow:
+			if (!forward)
+			{
+				characterArt.Rotate(0, 180, 0);
+				forward = true;
+			}
+			break;
+		}
+	}
+
+	void Start ()
+	{
+		UserInput.UserInputs += FlipCharacter;
+	}
+}
--- Star Catcher/Assets/Prototype #2/Scripts/Character Scripts/RabbitAnimation.cs
+++ Star Catcher/Assets/Prototype #2/Scripts/Character Scripts/RabbitAnimation.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class RabbitAnimation : MonoBehaviour {
 
-    private Animator anim;
+    private Animator myAnimator;
+	public PlayerControl playerControlScript;
 
 	// Use this for initialization
 	void Start () {
 
-        anim = GetComponent<Animator>();
+		playerControlScript = GetComponentInParent <PlayerControl> ();
+		myAnimator = GetComponent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        anim.SetBool("Idle", true);
+		if ((Input.GetKey (KeyCode.RightArrow)) || (Input.GetKey (KeyCode.LeftArrow)))
+			myAnimator.SetFloat ("RunSpeed", 1f);
+		else
+			myAnimator.SetFloat ("RunSpeed", 0f);
+
+		if (playerControlScript.myCC.isGrounded)
+			myAnimator.SetBool ("JumpTrue", false);
+		else
+			myAnimator.SetBool ("JumpTrue", true);
+
 
 	}
 }
