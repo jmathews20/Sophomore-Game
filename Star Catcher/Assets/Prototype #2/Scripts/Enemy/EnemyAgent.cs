99c39a2e2c759f74fed3e5f77b2e85c921bb34dd
Daffy <jmathews20@hotmail.com>
2016-10-27 15:34:16 -0600

    bug fixes

---
+++ Star Catcher/Assets/Prototype #2/Scripts/Enemy/EnemyAgent.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAgent : MonoBehaviour {
+
+	public NavMeshAgent myAgent;
+	public Transform player;
+
+	// Update is called once per frame
+	void Update () {
+		myAgent.destination = player.position;
+	}
+}
