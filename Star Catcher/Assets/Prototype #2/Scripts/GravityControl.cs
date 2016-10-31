89bba59fdd1f2d220cb99049d41e30ec582c03e3
Jared Mathews <jmathews20@hotmail.com>
2016-10-31 00:04:33 -0600

    Timer Script Added

---
+++ Star Catcher/Assets/Prototype #2/Scripts/GravityControl.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour
+{
+
+    CharacterController myCC;
+    Vector3 tempPos;
+    public float gravity = 1;
+    public float sideForce = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        myCC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos.y -= gravity * Time.deltaTime;
+        tempPos.z = sideForce;
+
+        if (myCC.isGrounded)
+        {
+            tempPos.y = 0;
+        }
+        print(myCC.velocity);
+        myCC.Move(tempPos);
+    }
+}
---
+++ Star Catcher/Assets/Prototype #2/Stars/CloneStar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneStar : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] stars;
+    public float spawnFrequency = 1;
+    public bool canSpawnStars = true;
+
+    private int i = 0;
+    private int j = 0;
+
+    IEnumerator SpawnStars()
+    {
+        while (canSpawnStars)
+        {
+            i = Random.Range(0, spawnPoints.Length - 1);
+            stars[j].SetActive(true);
+            stars[j].transform.position = spawnPoints[i].position;
+
+            if (j < stars.Length - 1)
+            {
+                j++;
+            }
+            else
+            {
+                j = 0;
+            }
+            //Instantiate(star, spawnPoints[i].position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnFrequency);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(SpawnStars());
+    }
+
+}
