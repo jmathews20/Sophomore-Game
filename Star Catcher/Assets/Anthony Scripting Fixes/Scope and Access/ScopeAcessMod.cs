7352c0d66b212d3c24243ee60ccfa11bf7f91985
Daffy <jmathews20@hotmail.com>
2016-11-11 15:25:54 -0700

    Fixing Grades

---
+++ Star Catcher/Assets/Anthony Scripting Fixes/Scope and Access/ScopeAcessMod.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAcessMod : MonoBehaviour {
+
+	public float carSpeed;
+	private Vector3 carLocation;
+
+
+	void Start()
+	{
+		
+	while (carLocation.x < 5)
+	{
+		transform.Translate(Vector3.right * carSpeed * Time.deltaTime);
+	}
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
