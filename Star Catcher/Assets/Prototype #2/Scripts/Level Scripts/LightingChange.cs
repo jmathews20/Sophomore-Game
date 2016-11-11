d87ce14314a20af6c00d5488c95eeaf38e99e58f
Jared <jmathews20@hotmail.com>
2016-11-11 14:29:11 -0700

    Visual Changes to game

---
+++ Star Catcher/Assets/Prototype #2/Scripts/Level Scripts/LightingChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightingChange : MonoBehaviour {
+
+	public Light myLight;
+	private Color newColor;
+	public float changeTime;
+	public int thing = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+
+		StartCoroutine(colorChange());
+		newColor = myLight.color;
+	
+	}
+
+
+	IEnumerator colorChange() {
+
+		Color colorPink = Color.magenta;
+		Color colorGreen = Color.green;
+
+		if (thing > 1)
+		yield return new WaitForSeconds (5);
+		colorLerp ();
+
+	}
+
+
+	void colorLerp()
+	{
+		myLight.color = Color.Lerp (myLight.color, newColor, Time.deltaTime * changeTime);
+	}
+}
